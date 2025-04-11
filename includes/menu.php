6fe287b8f1924d7b6dbf2fa59ce8e1a0ce1866ca
Fabio Silva <fabio@yusuketi.com>
2025-04-11 22:54:04 +0000

    Atualização do menu e navbar

--- includes/menu.php
+++ includes/menu.php
@@ -3,22 +3,22 @@ require_once 'functions.php';
 ?>
 <nav class="menu">
     <ul>
-        <li><a href="/gesp/pages/index.php">Início</a></li>
+        <li><a href="<?php echo SITE_URL; ?>/index.php">Início</a></li>
         
         <?php if (isColaborador()): ?>
-            <li><a href="/gesp/pages/projetos.php">Projetos</a></li>
-            <li><a href="/gesp/pages/tarefas.php">Tarefas</a></li>
-            <li><a href="/gesp/pages/relatorios.php">Relatórios</a></li>
+            <li><a href="<?php echo SITE_URL; ?>/pages/projetos.php">Projetos</a></li>
+            <li><a href="<?php echo SITE_URL; ?>/pages/tarefas.php">Tarefas</a></li>
+            <li><a href="<?php echo SITE_URL; ?>/pages/relatorios.php">Relatórios</a></li>
         <?php endif; ?>
 
         <?php if (isAdmin()): ?>
-            <li><a href="/gesp/pages/usuarios.php">Usuários</a></li>
-            <li><a href="/gesp/pages/configuracoes.php">Configurações</a></li>
+            <li><a href="<?php echo SITE_URL; ?>/pages/usuarios.php">Usuários</a></li>
+            <li><a href="<?php echo SITE_URL; ?>/pages/configuracoes.php">Configurações</a></li>
         <?php endif; ?>
         
         <li class="user-info">
             <span>Olá, <?php echo $_SESSION['nome']; ?> (<?php echo $_SESSION['perfil']; ?>)</span>
-            <a href="/gesp/logout.php">Sair</a>
+            <a href="<?php echo SITE_URL; ?>/logout.php">Sair</a>
         </li>
     </ul>
 </nav> 
--- includes/navbar.php
+++ includes/navbar.php
@@ -5,27 +5,27 @@ require_once __DIR__ . '/config.php';
 $current_path = $_SERVER['PHP_SELF'];
 $depth = substr_count($current_path, '/') - 2; // -2 para ajustar o caminho base
 //echo $depth;
-$home_path = '/gesp/index.php';
+$home_path = SITE_URL . '/index.php';
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="/gesp/index.php"><?php echo SITE_NAME; ?></a>
+        <a class="navbar-brand" href="<?php echo SITE_URL; ?>/index.php"><?php echo SITE_NAME; ?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav me-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/gesp/index.php">Dashboard</a>
+                    <a class="nav-link" href="<?php echo SITE_URL; ?>/index.php">Dashboard</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/gesp/pages/produtos.php">Produtos</a>
+                    <a class="nav-link" href="<?php echo SITE_URL; ?>/pages/produtos.php">Produtos</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/gesp/pages/movimentacoes.php">Movimentações</a>
+                    <a class="nav-link" href="<?php echo SITE_URL; ?>/pages/movimentacoes.php">Movimentações</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/gesp/pages/relatorios.php">Relatórios</a>
+                    <a class="nav-link" href="<?php echo SITE_URL; ?>/pages/relatorios.php">Relatórios</a>
                 </li>
                 <?php if (isAdmin()): ?>
                 <li class="nav-item dropdown">
@@ -33,18 +33,18 @@ $home_path = '/gesp/index.php';
                         Administração
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="/gesp/pages/unidades.php">Unidades</a></li>
-                        <li><a class="dropdown-item" href="/gesp/pages/localizacoes.php">Localizações</a></li>
-                        <li><a class="dropdown-item" href="/gesp/pages/usuarios.php">Usuários</a></li>
+                        <li><a class="dropdown-item" href="<?php echo SITE_URL; ?>/pages/unidades.php">Unidades</a></li>
+                        <li><a class="dropdown-item" href="<?php echo SITE_URL; ?>/pages/localizacoes.php">Localizações</a></li>
+                        <li><a class="dropdown-item" href="<?php echo SITE_URL; ?>/pages/usuarios.php">Usuários</a></li>
                         <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="/gesp/pages/backup.php">Backup</a></li>
+                        <li><a class="dropdown-item" href="<?php echo SITE_URL; ?>/pages/backup.php">Backup</a></li>
                     </ul>
                 </li>
                 <?php endif; ?>
             </ul>
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="/gesp/logout.php">Sair</a>
+                    <a class="nav-link" href="<?php echo SITE_URL; ?>/logout.php">Sair</a>
                 </li>
             </ul>
         </div>
